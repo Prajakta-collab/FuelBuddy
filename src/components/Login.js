60eadb05a3435066c9bad6ff609cb7b5ed0bdd14
Prajakta-collab <prajaktabadgujar2281@gmail.com>
2022-10-20 21:58:54 +0530

    .

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,57 +1,102 @@
-import React from 'react'
-
+import React,{useState} from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Dropdown from "react-bootstrap/Dropdown";
 const Login = () => {
+  const [value, setValue] = useState("Pump Owner");
+  const handleSelect = (e) => {
+    console.log("jdj")
+console.log(e);
+setValue(e);
+console.log(value);
+  }
   return (
     <div>
-        
-  <section className="vh-100 gradient-custom">
-  <div className="container py-5 h-80">
-    <div className="row d-flex justify-content-center align-items-center h-100">
-      <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-        <div className="card bg-dark text-white" >
-          <div className="card-body p-5 text-center">
+      <section className="vh-100 gradient-custom">
+        <div className="container py-5 h-80">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div className="card bg-dark text-white">
+                <div className="card-body p-5 text-center">
+                  <div className="mb-md-5 mt-md-4 pb-5">
+                    <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                    <p className="text-white-50 mb-5">
+                      Please enter your login and password!
+                    </p>
 
-            <div className="mb-md-5 mt-md-4 pb-5">
+                    <div class="dropdown form-outline  mb-4">
+                      
+                      <Dropdown onSelect={(e)=>{console.log(e)}}>
+                        <Dropdown.Toggle variant="primary" >
+                          Select User Type
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                          <Dropdown.Item value="pumpo">Pump Owner</Dropdown.Item>
+                          <Dropdown.Item value="trucko">Truck Owner</Dropdown.Item>
+                          <Dropdown.Item value="attendent">Attendant</Dropdown.Item>
+                        </Dropdown.Menu>
+                      </Dropdown>
+                    </div>
 
-              <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
-              <p className="text-white-50 mb-5">Please enter your login and password!</p>
+                    <div className="form-outline form-white mb-4">
+                      <input
+                        type="email"
+                        id="typeEmailX"
+                        placeholder="Email"
+                        className="form-control form-control-lg"
+                      />
+                    </div>
 
-              <div className="form-outline form-white mb-4">
-                <input type="email" id="typeEmailX" className="form-control form-control-lg" />
-                <label className="form-label" for="typeEmailX">Email</label>
+                    <div className="form-outline form-white mb-4">
+                      <input
+                        type="password"
+                        id="typePasswordX"
+                        placeholder="Password"
+                        className="form-control form-control-lg"
+                      />
+                    </div>
+
+                    <p className="small mb-5 pb-lg-2">
+                      <a className="text-white-50" href="#!">
+                        Forgot password?
+                      </a>
+                    </p>
+
+                    <button
+                      className="btn btn-outline-light btn-lg px-5"
+                      type="submit"
+                    >
+                      Login
+                    </button>
+
+                    <div className="d-flex justify-content-center text-center mt-4 pt-1">
+                      <a href="/" className="text-white">
+                        <i className="fab fa-facebook-f fa-lg"></i>
+                      </a>
+                      <a href="/" className="text-white">
+                        <i className="fab fa-twitter fa-lg mx-4 px-2"></i>
+                      </a>
+                      <a href="/" className="text-white">
+                        <i className="fab fa-google fa-lg"></i>
+                      </a>
+                    </div>
+                  </div>
+
+                  <div>
+                    <p className="mb-0">
+                      Don't have an account?{" "}
+                      <a href="#!" className="text-white-50 fw-bold">
+                        Sign Up
+                      </a>
+                    </p>
+                  </div>
+                </div>
               </div>
-
-              <div className="form-outline form-white mb-4">
-                <input type="password" id="typePasswordX" className="form-control form-control-lg" />
-                <label className="form-label" for="typePasswordX">Password</label>
-              </div>
-
-              <p className="small mb-5 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p>
-
-              <button className="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
-
-              <div className="d-flex justify-content-center text-center mt-4 pt-1">
-                <a href="/" className="text-white"><i className="fab fa-facebook-f fa-lg"></i></a>
-                <a href="/" className="text-white"><i className="fab fa-twitter fa-lg mx-4 px-2"></i></a>
-                <a href="/" className="text-white"><i className="fab fa-google fa-lg"></i></a>
-              </div>
-
             </div>
-
-            <div>
-              <p className="mb-0">Don't have an account? <a href="#!" className="text-white-50 fw-bold">Sign Up</a>
-              </p>
-            </div>
-
           </div>
         </div>
-      </div>
+      </section>
     </div>
-  </div>
-</section>
-     
-    </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
