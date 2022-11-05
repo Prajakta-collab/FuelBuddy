044367237d1cfb2c50de13653e16b4d9060aa411
isha <kawaleisha08@gmail.com>
2022-11-05 17:40:26 +0530

    pumpattendant

--- src/components/Hometo.js
+++ src/components/Hometo.js
@@ -8,7 +8,7 @@ const Hometo = () => {
       <div class="mt-3 mx-4">
         <p class="h4">Credit</p>
       </div>
-      <div class="table-responsive-sm pt-2 mx-5">
+      <div class="table-responsive-sm pt-2 mx-5 pe-5 me-5">
         <table class="table table-hover ">
           <thead>
             <tr class="table-primary">
@@ -28,7 +28,7 @@ const Hometo = () => {
       </div>
       <div class="mt-5 mx-4 mb-2">
         <p class="h4">Fuel Request</p>
-        <form class="border border-dark p-3">
+        <form class="border border-dark p-3" for>
           <div class="mb-3 mr-auto d-flex" >
             <div class="mx-3"><label class="form-label"><p class="h6">Fuel Quantity</p></label></div>
             <div class="w-75"><input type="text" class="form-control" placeholder=""></input></div>
@@ -37,7 +37,7 @@ const Hometo = () => {
             <div class="mx-3"><label class="form-label"><p class="h6">Vehicle Number</p></label></div>
             <div class="w-75"><input type="text" class="form-control"></input></div>
           </div>
-          <div class="text-center"><button type="submit" class="btn btn-primary">Submit</button></div>
+          <div class="text-center"><button type="submit" class="btn btn-primary" onclick="myFunction()">Submit</button></div>
         </form>
       </div>
     </div>
